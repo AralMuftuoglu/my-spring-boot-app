f09a17f746b96dce741163a1664c7dd99157d9ad
AralMuftuoglu <aralmuftuoglu@hotmail.com>
2025-07-24 22:34:43 +0300

    update

--- src/main/java/com/codewitharal/store/StoreApplication.java
+++ src/main/java/com/codewitharal/store/StoreApplication.java
@@ -13,3 +13,4 @@ public class StoreApplication {
         orderService.placeOrder();
     }
 }
+
--- src/main/java/com/codewitharal/store/StripePaymentService.java
+++ src/main/java/com/codewitharal/store/StripePaymentService.java
@@ -1,13 +1,36 @@
 package com.codewitharal.store;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 
 @Service("stripe")
 public class StripePaymentService implements PaymentService {
+
+    @Value("${stripe.apiUrl}")
+    private String apiUrl;
+
+    @Value("${stripe.enabled}")
+    private boolean enabled;
+
+    @Value("${stripe.timeout}")
+    private int timeout;
+
+    @Value("${stripe.supported-currencies}")
+    private List<String> supportedCurrencies;
+
     @Override
     public void processPayment(double amount){
         System.out.println("STRIPE");
         System.out.println("Amount: " + amount);
+
+
+        System.out.println("Currencies: " + supportedCurrencies);
+        System.out.println("Api Url: " + apiUrl);
+        System.out.println("Enabled: " + enabled);
+        System.out.println("Timeout: " + timeout);
+
     }
 }
